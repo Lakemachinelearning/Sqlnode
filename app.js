cebc01734c74fd7afc44c42fb63949ac31de0fd4
Bert Järv <bertjarv@gmail.com>
2019-05-27 00:50:45 +0200

    2.1

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-var mysql = require('mysql');
-console.log("Module created...");
+const express = require ("express");
+const mysql = require ("mysql");
+
+// Create connection
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456'
+});
+
+// Connect
+db.connect((err) => {
+  if(err){
+    throw err;
+  }
+  console.log("MySQL Connected...");
+});
+
+const app = express();
+
+// Create DB
+app.get('/createdb', (req, res) => {
+  let sql = "CREATE DATABASE nodemysql";
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send("Database created...");
+  });
+});
+
+app.listen("8080", () => {
+  console.log("Server started on port 8080..");
+
+});
